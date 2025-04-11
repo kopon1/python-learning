6706145da9bd4ce5af6754a0a7f569895d445bd3
gfigueroa07 <132857368+gfigueroa07@users.noreply.github.com>
2025-04-11 19:12:16 -0400

    .

--- dev/casper/calculator.py
+++ dev/casper/calculator.py
@@ -3,10 +3,9 @@
 
 # function for input validation
 def input_to_int():
-    num1 = input("Choose first number")
-    num2 = input("Choose second number")
+    num = float(input("Choose first number"))
     try:
-        num1.isdigit() and num2.isdigit()
+        num.isdigit()
     except ValueError as e:
         print("Invalid input. Please try again! ", e)
     
@@ -15,12 +14,13 @@ def input_to_int():
         
     except Exception:
         print("Error", e)
+validate = input_to_int()
 
 # function for adding
 def add(x,y):
-    answer = x + y
     num1 = input()
     num2 = input()
+    answer = num1 + num2
     return add(num1, num2)
 
 # function for multiplying
