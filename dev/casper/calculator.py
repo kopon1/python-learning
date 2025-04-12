690afbbe060fc187940137e42d15aab665fff993
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-04-12 18:39:46 -0400

    created two functions, one to prompt user to access calculator or exit program. The other one does the handling of adding, substraction, etc.

--- dev/casper/calculator.py
+++ dev/casper/calculator.py
@@ -30,17 +30,37 @@ def substract(x,y):
     return answer
 
 
-# TEST
-def choice():
+# function that handles user's choice to add, divide, multiply or substract
+def calc():
+    opt = input_to_int("Enter:\n1 to add\n2 to multiply\n3 to divide\n4 to substract")
+    while True:
+        if opt == 1:
+            num1 = input_to_int("Choose your first number ")
+            num2 = input_to_int("Choose your second number ")
+            print(add(num1, num2))
+        elif opt == 2:
+            num1 = input_to_int("Choose your first number ")
+            num2 = input_to_int("Choose your second number ")
+            print(multiply(num1, num2))
+        elif opt == 3:
+            num1 = input_to_int("Choose your first number ")
+            num2 = input_to_int("Choose your second number ")
+            print(divide(num1, num2))
+        elif opt == 4:
+            num1 = input_to_int("Choose your first number ")
+            num2 = input_to_int("Choose your second number ")
+            print(substract(num1, num2))
+        else:
+            print("Invalid choice, please try again")
+            
+# function that prompts user to access calculator or exit 
+
+def user_choice():
     while True:
         try:
             choice = input_to_int("What would you like to do? Enter 1 to access calculator")
             if choice == 1:
-                calc = input_to_int("What would you like to do? 1, for add, 2 for m, 3 for s, 4 div")
-                if calc == 1:
-                    print(add(num1, num2))
-                
-
+                calc()
             elif choice is None:
                 return
             else:
@@ -48,9 +68,19 @@ def choice():
         except Exception:
             print("Error")
                         
-choice()
-print("Hmm, quit? test test test")
+print(user_choice())
     
+
+
+
+
+
+
+
+
+
+
+
 
 # num1 = input_to_int("Choose your first number ")
 # num2 = input_to_int("Choose your second number ")
