06fd1e58ef6dea1d5d020871e517c0abe5cda1e7
gfigueroa07 <guillermofigueroa2840@gmail.com>
2025-05-09 17:59:25 -0400

    **IGNORE**

--- dev/casper/exercises/lists_and_arrays.py
+++ dev/casper/exercises/lists_and_arrays.py
@@ -2,12 +2,13 @@
 my_list = [1, 4, 39, 5, 20, 3]
 
 def find_num_in_list():
-    result= [ ]
-    for item in my_list:
-        if item == 5:
+    result = []
+    for x, element in enumerate(my_list):
+        if element == 5:
             break
-        result.append(item)
-        print(result)
-            
-            
+        elif 5 not in my_list:
+            print("Num 5 not in list")
+            return
+        result.append(element)
+    print(result)        
 find_num_in_list()
